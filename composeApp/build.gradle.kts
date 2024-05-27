616a9dfbcd6a55a7ad8176f061b9122e64156e88
limpid-kzonix <balyszyn@gmail.com>
2024-05-28 00:17:57 +0300

    feat(router): add skeleton of the application router

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(projects.shared)
+            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.7.0-alpha07")
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -4,11 +4,14 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +19,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.resources.painterResource
 
 import linguaspark.composeapp.generated.resources.Res
@@ -25,27 +34,131 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
+
+
+    // router
+    val navController = rememberNavController()
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                MainApp(navController = navController)
+            }
+            BottomNavigation {
+                var selected by remember { mutableStateOf(0) }
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
+                    label = { Text("Home") },
+                    selected = selected == 0,
+                    onClick = {
+                        navController.navigate(NavigationDestinations.HOME.route)
+                        selected = 0
+                    }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Person, contentDescription = "Profile") },
+                    label = { Text("Profile") },
+                    selected = selected == 1,
+                    onClick = {
+                        navController.navigate(NavigationDestinations.PROFILE.route)
+                        selected = 1
+                    }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Settings, contentDescription = "Settings") },
+                    label = { Text("Settings") },
+                    selected = selected == 2,
+                    onClick = {
+                        navController.navigate(NavigationDestinations.SETTINGS.route)
+                        selected = 2
+                    }
+                )
+                BottomNavigationItem(
+                    modifier = Modifier.background(MaterialTheme.colors.secondary),
+                    icon = { Icon(Icons.Filled.Info, contentDescription = "About") },
+                    label = { Text("About") },
+                    selected = selected == 3,
+                    onClick = {
+                        navController.navigate(NavigationDestinations.ABOUT.route)
+                        selected = 3
+                    }
+                )
             }
 
-            AnimatedVisibility(
-                visible = showContent,
-                enter = fadeIn() + slideInHorizontally(),
-                exit = fadeOut() + slideOutHorizontally()
-            ) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
         }
     }
+}
+
+@Composable
+fun MainApp(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable(NavigationDestinations.HOME.route) {
+            HomeScreen(navController)
+        }
+        composable(NavigationDestinations.PROFILE.route) {
+            ProfileScreen(navController)
+        }
+        composable(NavigationDestinations.SETTINGS.route) {
+            SettingsScreen(navController)
+        }
+        composable(NavigationDestinations.ABOUT.route) {
+            AboutScreen(navController)
+        }
+
+    }
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    // i need to center text in the middle of the screen
+    Box(
+        modifier = Modifier.fillMaxWidth().fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Home Screen")
+    }
+
+}
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    // box with row of two text elements
+    Box(
+        modifier = Modifier.fillMaxWidth().fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        )
+        {
+            Text("Profile Screen 1")
+            Text("Profile Screen 2")
+        }
+    }
+}
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Text("Settings Screen")
+}
+
+@Composable
+fun AboutScreen(navController: NavController) {
+    Text("About Screen")
+}
+
+enum class NavigationDestinations(val route: String) {
+    HOME("home"),
+    PROFILE("profile"),
+    SETTINGS("settings"),
+    ABOUT("about"),
 }
