695c7dba7d651b169aa634d2db5040e5b191bc13
limpid-kzonix <balyszyn@gmail.com>
2024-05-27 23:27:59 +0300

    Revert "yakasDich"
    This reverts commit 3367020aa0d2630227f72880cd2026ce36cddc8d.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,12 +1,4 @@
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.shrinkOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
@@ -24,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
 
 import linguaspark.composeapp.generated.resources.Res
@@ -36,21 +27,10 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        var yakasDich by remember { mutableStateOf(true) }
-        LaunchedEffect(yakasDich) {
-            if (yakasDich.not()) {
-                delay(500)
-                yakasDich = true
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
             }
-        }
-
-        AnimatedVisibility(yakasDich,
-            enter = scaleIn() + fadeIn(),
-            exit = slideOutHorizontally(animationSpec = tween(delayMillis = 500)) { +500 } + slideOutHorizontally(animationSpec = tween(delayMillis = 1000)) { -500 } + slideOutHorizontally(animationSpec = tween(delayMillis = 1500)) { +500 } + slideOutHorizontally(animationSpec = tween(delayMillis = 2000)) { -500 } + slideOutHorizontally(animationSpec = tween(delayMillis = 2500)) { +500 } + slideOutHorizontally() { -500 } + slideOutHorizontally(animationSpec = tween(delayMillis = 3000)) { +500 } + slideOutHorizontally() { -500 }
-        ) {
-            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                Button(onClick = { showContent = !showContent }) {
-                    Text("Click me!")
 
             AnimatedVisibility(
                 visible = showContent,
@@ -64,19 +44,6 @@ fun App() {
                 ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
-                }
-                Button(onClick = { yakasDich = !yakasDich }) {
-                    Text("Montana this is the mountain")
-                }
-                AnimatedVisibility(showContent) {
-                    val greeting = remember { Greeting().greet() }
-                    Column(
-                        Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Image(painterResource(Res.drawable.compose_multiplatform), null)
-                        Text("Compose: $greeting")
-                    }
                 }
             }
         }
